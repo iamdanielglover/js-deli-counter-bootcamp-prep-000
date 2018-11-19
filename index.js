308b360ffcd254d88981c10708861eeb8e51f73c
Daniel Glover <iamdanielglover@gmail.com>
2018-11-19 21:38:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,14 +12,6 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
-  var i = 0;
-  var incVar = i + 1;
-  
-  while (katzDeliLine.length > 0) {
-   "The line is currently: " + incVar +". " + katzDeliLine[i];
-    i++;
-  }
   if (katzDeliLine.length === 0) {
     return 'The line is currently empty.';
-  }
-}
+
