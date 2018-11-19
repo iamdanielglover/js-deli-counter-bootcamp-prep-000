a2a2f20dbc92fdbc9072b1d7174b0f549d84c02f
Daniel Glover <iamdanielglover@gmail.com>
2018-11-19 21:46:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function currentLine(katzDeliLine) {
   }
   var katzDeliLineNamesAndNumbers = [];
   for(var i = 0; i < katzDeliLine.length; i++) {
-    katzDeliLineNamesAndNumbers.push(i+1 + '. ' + katzDeliLine[i] " ");
+    katzDeliLineNamesAndNumbers.push(i+1 + '. ' + katzDeliLine[i] + " ");
   }
   return "The line is currently: " + katzDeliLineNamesAndNumbers;
 }
